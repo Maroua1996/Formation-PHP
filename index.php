43b1204171224d5eb61d9b0d78517135c7a0783a
Maroua Bihich <bihich.maroua@gmail.com>
2021-03-27 13:38:29 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8" />
+</head>
+
+<body> <?php print('hola yosra est une copieuse <br /> ');
+
+echo 'ceci est un autre test <br/>';
+//Ceci est mon premire fichier php de ce dossier ?>
+
+    <?php print('<b>je sui fachés</b> <br /> ');
+
+echo 'ceci est un\"autre\" test ';
+//Ceci est mon premire fichier php de ce dossier ?>
+
+
+
+</body>
+
+
+</html>
